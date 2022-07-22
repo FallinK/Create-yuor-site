52d43d18406788b58ef390eb45a0edc1e0d57215
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-07-22 15:24:00 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict"
+// SCROLL
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo ({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+//BURGER
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+//Animation Button Switch
+const sw = document.querySelector('.switch');
+const swBtn = document.querySelector('.switch__btn');
+if (sw) {
+	swBtn.addEventListener("click", e => {
+		swBtn.classList.toggle('_moove');
+		sw.classList.toggle('_active');
+	});
+}
